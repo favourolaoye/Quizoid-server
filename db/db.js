05b57f236339778da95cea241ccf3a964d57085a
0x46641766F7572 <96478305+favourolaoye@users.noreply.github.com>
2024-08-30 20:28:04 +0100

    updates

--- db/db.js
+++ db/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/exam-system', {
+    await mongoose.connect(process.env.MONGO_URI, {
     });
     console.log('MongoDB connected');
   } catch (err) {
