d607fbc94cf5cbc32a9c9a780777d38711cb1771
0x46641766F7572 <96478305+favourolaoye@users.noreply.github.com>
2024-08-30 20:35:53 +0100

    updates

--- controllers/lecturerController.js
+++ controllers/lecturerController.js
@@ -1,6 +1,6 @@
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-import Lecturer from '../models/Lecturer.js';
+import Lecturer from '../models/lecturer.js';
 
 
 const secret = process.env.SECRET_ID;
